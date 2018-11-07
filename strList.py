d2e25c85f937bcb14f4c655305d542000b2271a0
Santiago Cortez <sanfctz@gmail.com>
2018-11-07 12:27:19 -0300

    [Add] List, Tupples

---
+++ strList.py
@@ -0,0 +1,13 @@
+name = 'Santi'
+#Check for the index
+print(name[0])
+print(name[-2])
+print(name[0:4])
+
+
+i ='Sa' in name
+print(i)
+
+for i in name:
+    print('***' + i + '***')
+
---
+++ tupples.py
@@ -0,0 +1,9 @@
+#Tuples are typed with parenthesis and are immutable
+eggs = ('hello',42,0.5)
+#Create a tuple with only one value
+type(('hello',))
+
+#Convert types
+tuple(['cat','dot',5]) #Convert list to tuple
+list(('cat','dog',5)) #Convert tuple to list
+
